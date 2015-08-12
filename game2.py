dc330288c2ab9552f195d7a090defabaffe713bf
lexwraith <michael@modernliteracy.org>
2015-08-12 18:22:06 -0300

    Map fully functional. Now for the orcies.

--- game2.py
+++ game2.py
@@ -32,16 +32,26 @@ class Game:
 
         # Hidden work surface for all the calculations/changes
         self.canvas = l.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+        # Secondary layer for field of vision purposes
+        self.fov = l.map_new(MAP_WIDTH, MAP_HEIGHT)
+        self.fovRecompute = True
         
         # Set up game/map here
         self.spawnPlayer()
         self.makeMap()
-        
+       
+        for y in range(MAP_HEIGHT):
+            for x in range(MAP_WIDTH):
+                l.map_set_properties(self.fov,
+                    x, y, not self.map[x][y].block_sight,
+                    not self.map[x][y].blocked)
+ 
         # Blit is what updates the main surface to your work surface
         l.console_blit(self.canvas, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
         
         while(self.state):
-            #self.renderAll()
+            self.renderAll()
 
             l.console_blit(self.canvas, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
             l.console_flush()        
@@ -78,6 +88,33 @@ class Game:
                 return True
         return False
 
+    def genObjects(self, room):
+        """
+        Generates stuff in a room.
+
+        TODO: Generalize this for all first-time generation of a room.
+        """
+        numMonsters = randint(0, 0, MAX_ROOM_MONSTERS)
+        for i in range(numMonsters):
+            x = randint(0, room.x1, room.x2)
+            y = randint(0, room.y1, room.y2)
+            if not self.isBlocked(x,y):    
+                if(randint(0, 0, 100) < 80): #80% chance
+                    fighter_component = Fighter(3, 3, 3, death_function=monster_death)
+                    ai_component = BasicMonster()
+                    monster = Object(self, x, y, 'o', 
+                        'orc', libtcod.desaturated_green, blocks=True,
+                        fighter=fighter_component,
+                        ai=ai_component)
+                else:
+                    fighter_component = Fighter(4, 4, 4, death_function=monster_death)
+                    ai_component = BasicMonster()
+                    monster = Object(self, x, y, 'T', 
+                        'troll', libtcod.darker_green, blocks=True,
+                        fighter=fighter_component,
+                        ai=ai_component)
+                self.objects.append(monster)
+        
     def spawnPlayer(self):
         """
         Only allows one instance of a player.
@@ -97,13 +134,30 @@ class Game:
             self.state = 0
             return 'exit'
         if(self.state):
-            if(l.console_is_key_pressed(l.KEY_UP)):          self.player.move(0,-1)
-            elif(l.console_is_key_pressed(l.KEY_DOWN)):      self.player.move(0,1)
-            elif(l.console_is_key_pressed(l.KEY_LEFT)):      self.player.move(-1,0)
-            elif(l.console_is_key_pressed(l.KEY_RIGHT)):     self.player.move(0,1)
+            if(l.console_is_key_pressed(l.KEY_UP)):          self.playerMove(0,-1)
+            elif(l.console_is_key_pressed(l.KEY_DOWN)):      self.playerMove(0,1)
+            elif(l.console_is_key_pressed(l.KEY_LEFT)):      self.playerMove(-1,0)
+            elif(l.console_is_key_pressed(l.KEY_RIGHT)):     self.playerMove(1,0)
             else:
                 pass
 
+    def playerMove(self, dx, dy):
+        x = self.player.x + dx
+        y = self.player.y + dy
+
+        target = None
+        for obj in self.objects:
+            if obj.fighter and obj.x == x and obj.y == y:
+                target = obj
+                break
+        if target is not None:
+            self.player.fighter.attack(target)
+        else:
+            self.player.move(dx, dy)
+            self.fovRecompute = True
+    ##################################
+    # MAP STUFF
+    ##################################
     def createRoom(self, room):
         for x in range(room.x1 + 1, room.x2 + 1):
             for y in range(room.y1 + 1, room.y2 + 1):
@@ -172,5 +226,46 @@ class Game:
                 self.rooms.append(new_room)
                 self.numRooms += 1
 
+    def renderAll(self):
+        if self.fovRecompute:
+            self.fovRecompute = False
+            l.map_compute_fov(self.fov, 
+                self.player.x, self.player.y, 
+                TORCH_RADIUS,
+                FOV_LIGHT_WALLS, FOV_ALGO)
+        
+            for y in range(MAP_HEIGHT):
+                for x in range(MAP_WIDTH):
+                    visible = l.map_is_in_fov(self.fov, x, y)
+                    wall = self.map[x][y].block_sight
+                    if not visible:
+                        if self.map[x][y].explored:
+                            if wall:
+                                l.console_set_char_background(self.canvas, 
+                                    x, y, color_dark_wall, l.BKGND_SET)
+                            else:
+                                l.console_set_char_background(self.canvas, 
+                                    x, y, color_dark_ground, l.BKGND_SET)
+                    else:
+                        if wall:
+                            l.console_set_char_background(self.canvas, 
+                                x, y, color_light_wall, l.BKGND_SET )
+                        else:
+                            l.console_set_char_background(self.canvas, 
+                                x, y, color_light_ground, l.BKGND_SET ) 
+                        self.map[x][y].explored = True
+
+        for obj in self.objects:
+            if(obj != self.player):
+                obj.draw()
+        self.player.draw()
+
+        l.console_blit(self.canvas, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+
+    #l.console_set_default_foreground(con, libtcod.white)
+    #l.console_print_ex(con, 1, SCREEN_HEIGHT - 2, libtcod.BKGND_NONE, libtcod.LEFT,
+    #    'HP:%s/%s' % (player.fighter.hp, player.fighter.max_hp))
+    
+             
 if __name__ == "__main__":
     g = Game("Hi")
